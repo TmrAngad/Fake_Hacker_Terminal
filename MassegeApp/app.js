aba17fbd3c7da2b85ed94c0eb5909331c054ec61
TmrAngad <tmrangad137@gmail.com>
2024-02-13 21:10:24 +0530

    Masseging App

---
+++ MassegeApp/app.js
@@ -0,0 +1,59 @@
+
+        //Masseging app
+
+        //msg input variable 
+        let leftMsgCon;
+        let rightMsgCon;
+
+        //msg output variable 
+        let leftCon;
+        let rightCon;
+
+        // msg send button variable
+
+        let leftbtn = document.querySelector('.leftbtn')
+        let rightbtn = document.querySelector('.rightbtn')
+
+        const leftMsg = () => {
+            leftMsgCon = document.querySelector('.leftMsgCon')
+            rightCon = document.querySelector('.rightReadMsgCon')
+            let leftshowCon = `
+            <div class="msgChildCon borderBox addPadding fontFamily style="margin-left: 6em;background-color: black;color: green;">
+                    ${leftMsgCon.value}
+                </div>
+            
+            `
+            let rightshowCon = `
+            <div class="msgChildCon borderBox addPadding fontFamily" style="margin-left: 1em;background-color: antiquewhite;color: black;">
+                ${leftMsgCon.value}
+                </div>
+            
+            `
+            leftCon = document.querySelector('.leftReadMsgCon').innerHTML += leftshowCon;
+            leftMsgCon.value = ""
+            rightCon = document.querySelector('.rightReadMsgCon').innerHTML += rightshowCon;
+
+        }
+        const rightMsg = () => {
+            rightMsgCon = document.querySelector('.rightMsgCon')
+            let leftshowCon = `
+            <div class="msgChildCon borderBox addPadding fontFamily style="margin-left: 6em;background-color: black;color: green;">
+                    ${rightMsgCon.value}
+                </div>
+            
+            `
+            let rightshowCon = `
+            <div class="msgChildCon borderBox addPadding fontFamily" style="margin-left: 1em;background-color: antiquewhite;color: black;">
+                ${rightMsgCon.value}
+                </div>`
+            leftCon = document.querySelector('.leftReadMsgCon').innerHTML += rightshowCon;
+            rightCon = document.querySelector('.rightReadMsgCon').innerHTML += leftshowCon;
+            rightMsgCon.value = "";
+        }
+
+        leftbtn.addEventListener('click', () => {
+            leftMsg()
+        })
+        rightbtn.addEventListener('click', () => {
+            rightMsg()
+        })
